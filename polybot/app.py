50ecee69463c9006572fda8262cb265f388e2de9
Anas-Mhameed <mhameedanaskhader@gmail.com>
2024-04-14 20:27:16 +0300

    Add flask main service

---
+++ polybot/app.py
@@ -0,0 +1,34 @@
+import flask
+from flask import request
+import os
+from bot import ObjectDetectionBot, Bot, QuoteBot
+
+app = flask.Flask(__name__)
+
+
+TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+#my_token_file = os.environ('TELEGRAM_TOKEN_FILE')
+
+TELEGRAM_TOKEN = ""
+with open('/run/secrets/telegram_token_file', 'r') as file:
+    TELEGRAM_TOKEN = ''.join(file.read().split('\n'))
+    print(TELEGRAM_TOKEN)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Ok'
+
+
+@app.route(f'/{TELEGRAM_TOKEN}/', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    bot.handle_message(req['message'])
+    return 'Ok'
+
+
+if __name__ == "__main__":
+    bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
+#    bot = Bot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
+
+    app.run(host='0.0.0.0', port=8443)
+#   TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
